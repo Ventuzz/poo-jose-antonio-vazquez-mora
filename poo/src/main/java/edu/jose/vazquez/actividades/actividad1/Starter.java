e4301778e6e3d2b21affbc63e72b63df3ad59c0a
Ventuzz <mariosora3@gmail.com>
2025-01-17 22:10:01 -0600

    Documentación

--- poo/src/main/java/edu/jose/vazquez/actividades/actividad1/Starter.java
+++ poo/src/main/java/edu/jose/vazquez/actividades/actividad1/Starter.java
@@ -2,6 +2,10 @@ package edu.jose.vazquez.actividades.actividad1;
 
 import edu.jose.vazquez.actividades.actividad1.ui.CLI;
 
+/**
+ * Clase principal para iniciar la aplicación.
+ */
+
 public class Starter {
         public static void main(String[] args) {
         CLI.runApp();
--- poo/src/main/java/edu/jose/vazquez/actividades/actividad1/models/Car.java
+++ poo/src/main/java/edu/jose/vazquez/actividades/actividad1/models/Car.java
@@ -1,11 +1,21 @@
 package edu.jose.vazquez.actividades.actividad1.models;
 
+/**
+ * Clase que representa un carro con los aributos de modelo, marca, placa y color.
+ * Proporciona métodos para obtener y modificar los atributos del carro.
+ * También proporciona un método para mostrar la información del carro.
+ */
+
 public class Car {
     private String modelo;
     private String marca;
     private String placa;
     private String color;
 
+    /**
+     * Representa el carro en forma de texto con un diseño visual en forma de menú.
+     * @return una cadena de texto con la información del carro.
+     */
     public String toString() {
         return "╔═══════════════════╗\n" +
                "║       Carro       ║\n" +
@@ -17,7 +27,15 @@ public class Car {
                "║ Color  : " + color + "     ║\n" +
                "╚═══════════════════╝";
     }
-    
+
+    /**
+     * Constructor de la clase Car que cuenta con los siguientes parametros:
+     * @param modelo
+     * @param marca
+     * @param placa
+     * @param color
+     */
+
     public Car(String modelo, String marca, String placa, String color){
         this.modelo = modelo;
         this.marca = marca;
@@ -25,6 +43,10 @@ public class Car {
         this.color = color;
     }
 
+    /**
+     * Obtiene la placa del carro
+     * @return la placa del carro
+     */
     public String getPlaca(){
         return placa;
     }
